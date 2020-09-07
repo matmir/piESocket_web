70840dab2ee97c2dcbfa897beb5262f1fdd8896b
Mateusz Miroslawski <mateusz@s-mail.pl>
2020-09-07 11:40:38 +0200

    Update tests

--- tests/BaseFunctionTestCase.php
+++ tests/BaseFunctionTestCase.php
@@ -33,7 +33,8 @@ abstract class BaseFunctionTestCase extends WebTestCase {
         
         // Clear test server process data
         $this->parser->setBit('BIT_CLEAR_PROCESS');
-        $this->waitOnProcessDataSync();
+        $this->parser->setBit('MB_BIT_CLEAR_PROCESS');
+        $this->waitOnProcessDataSync(false);
     }
     
     public function tearDown() {
@@ -42,10 +43,19 @@ abstract class BaseFunctionTestCase extends WebTestCase {
         $this->parser = null;
     }
     
+    protected function waitOnProcessDataSync(bool $onlyShm=true) {
+        
+        $this->waitOnShmProcessDataSync();
+        
+        if (!$onlyShm) {
+            $this->waitOnModbusProcessDataSync();
+        }
+    }
+    
     /**
-     * Wait until process data is updated in SHM
+     * Wait until process data is updated in SHM driver
      */
-    protected function waitOnProcessDataSync() {
+    protected function waitOnShmProcessDataSync() {
         
         $this->parser->setBit('BIT_SYNC');
         while (!$this->parser->getBit('BIT_SYNC')) {
@@ -54,6 +64,22 @@ abstract class BaseFunctionTestCase extends WebTestCase {
         
         $this->parser->resetBit('BIT_SYNC');
         while ($this->parser->getBit('BIT_SYNC')) {
+            usleep(10000);
+        }
+    }
+    
+    /**
+     * Wait until process data is updated in Modbus driver
+     */
+    protected function waitOnModbusProcessDataSync() {
+        
+        $this->parser->setBit('MB_BIT_SYNC');
+        while (!$this->parser->getBit('MB_BIT_SYNC')) {
+            usleep(10000);
+        }
+        
+        $this->parser->resetBit('MB_BIT_SYNC');
+        while ($this->parser->getBit('MB_BIT_SYNC')) {
             usleep(10000);
         }
     }
--- tests/function/ProcessDataManipulationTest.php
+++ tests/function/ProcessDataManipulationTest.php
@@ -19,16 +19,24 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
     public function testGetBit() {
         
         // 0101 1011
-        $pret = $this->parser->writeByte('TEST_BYTE10', 91);
+        $pret1 = $this->parser->writeByte('TEST_BYTE10', 91);
+        // 1101 1001
+        $pret2 = $this->parser->writeByte('MB_TEST_BYTE40', 217);
         
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
         
-        $this->waitOnProcessDataSync();
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(1, $this->parser->getBit('TEST_BIT0'));
         $this->assertEquals(1, $this->parser->getBit('TEST_BIT1'));
         $this->assertEquals(0, $this->parser->getBit('TEST_BIT2'));
         $this->assertEquals(1, $this->parser->getBit('TEST_BIT3'));
+        
+        $this->assertEquals(1, $this->parser->getBit('MB_TEST_BIT0'));
+        $this->assertEquals(0, $this->parser->getBit('MB_TEST_BIT1'));
+        $this->assertEquals(0, $this->parser->getBit('MB_TEST_BIT2'));
+        $this->assertEquals(1, $this->parser->getBit('MB_TEST_BIT3'));
     }
     
     public function testGetBitWrong1() {
@@ -53,13 +61,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
     public function testGetBits() {
         
         // 0101 1011
-        $pret = $this->parser->writeByte('TEST_BYTE10', 91);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeByte('TEST_BYTE10', 91);
+        // 1101 1001
+        $pret2 = $this->parser->writeByte('MB_TEST_BYTE40', 217);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         // Prepare tag names array
-        $tags = array('TEST_BIT4', 'TEST_BIT5', 'TEST_BIT6', 'TEST_BIT7');
+        $tags = array('TEST_BIT4', 'TEST_BIT5', 'TEST_BIT6', 'TEST_BIT7',
+                        'MB_TEST_BIT4', 'MB_TEST_BIT5', 'MB_TEST_BIT6', 'MB_TEST_BIT7');
         
         $vals = $this->parser->getBits($tags);
         
@@ -67,6 +80,11 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         $this->assertEquals(0, $vals[1]);
         $this->assertEquals(1, $vals[2]);
         $this->assertEquals(0, $vals[3]);
+        
+        $this->assertEquals(1, $vals[4]);
+        $this->assertEquals(0, $vals[5]);
+        $this->assertEquals(1, $vals[6]);
+        $this->assertEquals(1, $vals[7]);
     }
     
     public function testGetBitsWrong1() {
@@ -96,14 +114,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process byte
         $this->assertEquals(0, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_BYTE40'));
         
-        $pret = $this->parser->setBit('TEST_BIT1');
+        $pret1 = $this->parser->setBit('TEST_BIT1');
+        $pret2 = $this->parser->setBit('MB_TEST_BIT5');
         
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
         
-        $this->waitOnProcessDataSync();
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(1, $this->parser->getBit('TEST_BIT1'));
+        $this->assertEquals(1, $this->parser->getBit('MB_TEST_BIT5'));
     }
     
     public function testSetBitWrong1() {
@@ -128,20 +150,30 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
     public function testResetBit() {
         
         // 0101 1011
-        $pret1 = $this->parser->writeByte('TEST_BYTE10', 91);
-        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $pret_shm1 = $this->parser->writeByte('TEST_BYTE10', 91);
+        // 1101 1001
+        $pret_mb1 = $this->parser->writeByte('MB_TEST_BYTE40', 217);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret_shm1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret_mb1);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(91, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(217, $this->parser->getByte('MB_TEST_BYTE40'));
         
         // 0100 1011
-        $pret2 = $this->parser->resetBit('TEST_BIT4');
-        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        $pret_shm2 = $this->parser->resetBit('TEST_BIT4');
+        // 1101 0001
+        $pret_mb2 = $this->parser->resetBit('MB_TEST_BIT3');
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret_shm2);
+        $this->assertEquals(ParserReplyCodes::OK, $pret_mb2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(75, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(209, $this->parser->getByte('MB_TEST_BYTE40'));
     }
     
     public function testResetBitWrong1() {
@@ -167,16 +199,19 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process byte
         $this->assertEquals(0, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_BYTE40'));
         
         // Prepare tag names array
-        $tags = array('TEST_BIT0', 'TEST_BIT5', 'TEST_BIT7');
+        $tags = array('TEST_BIT0', 'TEST_BIT5', 'TEST_BIT7',
+                        'MB_TEST_BIT2', 'MB_TEST_BIT4', 'MB_TEST_BIT6');
         
         $pret = $this->parser->setBits($tags);
         $this->assertEquals(ParserReplyCodes::OK, $pret);
         
-        $this->waitOnProcessDataSync();
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(161, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(84, $this->parser->getByte('MB_TEST_BYTE40'));
     }
     
     public function testSetBitsWrong1() {
@@ -208,13 +243,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process byte
         $this->assertEquals(0, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_BYTE40'));
         
-        $pret = $this->parser->invertBit('TEST_BIT1');
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->invertBit('TEST_BIT1');
+        $pret2 = $this->parser->invertBit('MB_TEST_BIT5');
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(2, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(32, $this->parser->getByte('MB_TEST_BYTE40'));
     }
     
     public function testInvertBitWrong1() {
@@ -240,13 +280,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process byte
         $this->assertEquals(0, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_BYTE40'));
         
-        $pret = $this->parser->invertBit('TEST_BIT2');
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->invertBit('TEST_BIT2');
+        $pret2 = $this->parser->invertBit('MB_TEST_BIT1');
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(4, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(2, $this->parser->getByte('MB_TEST_BYTE40'));
     }
     
     public function testGetByteWrong1() {
@@ -274,13 +319,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process byte
         $this->assertEquals(0, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_BYTE40'));
         
-        $pret = $this->parser->writeByte('TEST_BYTE10', 58);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeByte('TEST_BYTE10', 58);
+        $pret2 = $this->parser->writeByte('MB_TEST_BYTE40', 64);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(58, $this->parser->getByte('TEST_BYTE10'));
+        $this->assertEquals(64, $this->parser->getByte('MB_TEST_BYTE40'));
     }
     
     public function testWriteByteWrong1() {
@@ -312,13 +362,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process word
         $this->assertEquals(0, $this->parser->getWord('TEST_WORD10'));
+        $this->assertEquals(0, $this->parser->getWord('MB_TEST_WORD40'));
         
-        $pret = $this->parser->writeByte('TEST_BYTE11', 132);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeByte('TEST_BYTE11', 132);
+        $pret2 = $this->parser->writeByte('MB_TEST_BYTE41', 74);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(33792, $this->parser->getWord('TEST_WORD10'));
+        $this->assertEquals(18944, $this->parser->getWord('MB_TEST_WORD40'));
     }
     
     public function testGetWordWrong1() {
@@ -346,15 +401,23 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process word
         $this->assertEquals(0, $this->parser->getWord('TEST_WORD10'));
+        $this->assertEquals(0, $this->parser->getWord('MB_TEST_WORD40'));
         
-        $pret = $this->parser->writeWord('TEST_WORD10', 27900);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeWord('TEST_WORD10', 27900);
+        $pret2 = $this->parser->writeWord('MB_TEST_WORD40', 55843);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(27900, $this->parser->getWord('TEST_WORD10'));
         $this->assertEquals(252, $this->parser->getByte('TEST_BYTE10'));
         $this->assertEquals(108, $this->parser->getByte('TEST_BYTE11'));
+        
+        $this->assertEquals(55843, $this->parser->getWord('MB_TEST_WORD40'));
+        $this->assertEquals(35, $this->parser->getByte('MB_TEST_BYTE40'));
+        $this->assertEquals(218, $this->parser->getByte('MB_TEST_BYTE41'));
     }
     
     public function testWriteWordWrong1() {
@@ -386,13 +449,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process dword
         $this->assertEquals(0, $this->parser->getDWord('TEST_DWORD10'));
+        $this->assertEquals(0, $this->parser->getDWord('MB_TEST_DWORD40'));
         
-        $pret = $this->parser->writeWord('TEST_WORD12', 27901);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeWord('TEST_WORD12', 27901);
+        $pret2 = $this->parser->writeWord('MB_TEST_WORD42', 55844);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(1828519936, $this->parser->getDWord('TEST_DWORD10'));
+        $this->assertEquals(3659792384, $this->parser->getDWord('MB_TEST_DWORD40'));
     }
     
     public function testGetDWordWrong1() {
@@ -420,15 +488,23 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process dword
         $this->assertEquals(0, $this->parser->getDWord('TEST_DWORD10'));
+        $this->assertEquals(0, $this->parser->getDWord('MB_TEST_DWORD40'));
         
-        $pret = $this->parser->writeDWord('TEST_DWORD10', 1828520016);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeDWord('TEST_DWORD10', 1828520016);
+        $pret2 = $this->parser->writeDWord('MB_TEST_DWORD40', 3659793152);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(1828520016, $this->parser->getDWord('TEST_DWORD10'));
         $this->assertEquals(80, $this->parser->getWord('TEST_WORD10'));
         $this->assertEquals(27901, $this->parser->getWord('TEST_WORD12'));
+        
+        $this->assertEquals(3659793152, $this->parser->getDWord('MB_TEST_DWORD40'));
+        $this->assertEquals(768, $this->parser->getWord('MB_TEST_WORD40'));
+        $this->assertEquals(55844, $this->parser->getWord('MB_TEST_WORD42'));
     }
     
     public function testWriteDWordWrong1() {
@@ -460,13 +536,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process int
         $this->assertEquals(0, $this->parser->getInt('TEST_INT14'));
+        $this->assertEquals(0, $this->parser->getInt('MB_TEST_INT40'));
         
-        $pret = $this->parser->writeInt('TEST_INT14', -3789);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeInt('TEST_INT14', -3789);
+        $pret2 = $this->parser->writeInt('MB_TEST_INT40', -6773);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(-3789, $this->parser->getInt('TEST_INT14'));
+        $this->assertEquals(-6773, $this->parser->getInt('MB_TEST_INT40'));
     }
     
     public function testGetIntWrong1() {
@@ -512,13 +593,18 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process real
         $this->assertEquals(0, $this->parser->getReal('TEST_REAL30'));
+        $this->assertEquals(0, $this->parser->getReal('MB_TEST_REAL44'));
         
-        $pret = $this->parser->writeReal('TEST_REAL30', -789.048);
-        $this->assertEquals(ParserReplyCodes::OK, $pret);
+        $pret1 = $this->parser->writeReal('TEST_REAL30', -789.048);
+        $pret2 = $this->parser->writeReal('MB_TEST_REAL44', -106.007);
         
-        $this->waitOnProcessDataSync();
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
         
         $this->assertEquals(-789.048, $this->parser->getReal('TEST_REAL30'));
+        $this->assertEquals(-106.007, $this->parser->getReal('MB_TEST_REAL44'));
     }
     
     public function testGetRealWrong1() {
@@ -555,5 +641,72 @@ class ProcessDataManipulationTest extends BaseFunctionTestCase {
         
         // Check process int
         $this->assertEquals(0, $this->parser->writeReal('TEST_WORD12', -1359.78));
+    }
+    
+    public function testInputData1() {
+        
+        // Check SHM input data
+        $this->assertEquals(0, $this->parser->getBit('TEST_LOG_BIT2'));
+        $this->assertEquals(0, $this->parser->getByte('TEST_LOG_BYTE1'));
+        $this->assertEquals(0, $this->parser->getWord('TEST_LOG_WORD1'));
+        $this->assertEquals(0, $this->parser->getDWord('TEST_LOG_DWORD1'));
+        $this->assertEquals(0, $this->parser->getInt('TEST_LOG_INT1'));
+        $this->assertEquals(0, $this->parser->getReal('TEST_LOG_REAL1'));
+        
+        // Check Modbus input data
+        $this->assertEquals(0, $this->parser->getBit('MB_TEST_LOG_BIT1'));
+        $this->assertEquals(0, $this->parser->getByte('MB_TEST_LOG_BYTE1'));
+        $this->assertEquals(0, $this->parser->getWord('MB_TEST_LOG_WORD1'));
+        $this->assertEquals(0, $this->parser->getDWord('MB_TEST_LOG_DWORD1'));
+        $this->assertEquals(0, $this->parser->getInt('MB_TEST_LOG_INT1'));
+        $this->assertEquals(0, $this->parser->getReal('MB_TEST_LOG_REAL1'));
+        
+        // Activate first data set
+        $tags1 = array('TEST_LOG_SIM1', 'MB_TEST_LOG_SIM1');
+        
+        $pret1 = $this->parser->setBits($tags1);
+        $this->assertEquals(ParserReplyCodes::OK, $pret1);
+        
+        $this->waitOnProcessDataSync(false);
+        
+        // Check SHM input data
+        $this->assertEquals(0, $this->parser->getBit('TEST_LOG_BIT2'));
+        $this->assertEquals(55, $this->parser->getByte('TEST_LOG_BYTE1'));
+        $this->assertEquals(12120, $this->parser->getWord('TEST_LOG_WORD1'));
+        $this->assertEquals(558654, $this->parser->getDWord('TEST_LOG_DWORD1'));
+        $this->assertEquals(-1201, $this->parser->getInt('TEST_LOG_INT1'));
+        $this->assertEquals(3.79, $this->parser->getReal('TEST_LOG_REAL1'));
+        
+        // Check Modbus input data
+        $this->assertEquals(1, $this->parser->getBit('MB_TEST_LOG_BIT1'));
+        $this->assertEquals(210, $this->parser->getByte('MB_TEST_LOG_BYTE1'));
+        $this->assertEquals(20478, $this->parser->getWord('MB_TEST_LOG_WORD1'));
+        $this->assertEquals(784566, $this->parser->getDWord('MB_TEST_LOG_DWORD1'));
+        $this->assertEquals(-410, $this->parser->getInt('MB_TEST_LOG_INT1'));
+        $this->assertEquals(3.07, $this->parser->getReal('MB_TEST_LOG_REAL1'));
+        
+        // Activate second data set
+        $tags2 = array('TEST_LOG_DATA1', 'MB_TEST_LOG_DATA1');
+        
+        $pret2 = $this->parser->setBits($tags2);
+        $this->assertEquals(ParserReplyCodes::OK, $pret2);
+        
+        $this->waitOnProcessDataSync(false);
+        
+        // Check SHM input data
+        $this->assertEquals(1, $this->parser->getBit('TEST_LOG_BIT2'));
+        $this->assertEquals(150, $this->parser->getByte('TEST_LOG_BYTE1'));
+        $this->assertEquals(35120, $this->parser->getWord('TEST_LOG_WORD1'));
+        $this->assertEquals(158654, $this->parser->getDWord('TEST_LOG_DWORD1'));
+        $this->assertEquals(-1200, $this->parser->getInt('TEST_LOG_INT1'));
+        $this->assertEquals(3.78, $this->parser->getReal('TEST_LOG_REAL1'));
+        
+        // Check Modbus input data
+        $this->assertEquals(0, $this->parser->getBit('MB_TEST_LOG_BIT1'));
+        $this->assertEquals(37, $this->parser->getByte('MB_TEST_LOG_BYTE1'));
+        $this->assertEquals(42001, $this->parser->getWord('MB_TEST_LOG_WORD1'));
+        $this->assertEquals(158653, $this->parser->getDWord('MB_TEST_LOG_DWORD1'));
+        $this->assertEquals(-1300, $this->parser->getInt('MB_TEST_LOG_INT1'));
+        $this->assertEquals(9.78, $this->parser->getReal('MB_TEST_LOG_REAL1'));
     }
 }
